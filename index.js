b188e55d5b3205802ad7c558de4cdbbc1ee44153
lanmower <lanmower@users.noreply.github.com>
2025-11-11 14:47:11 +0200

    Initial commit: HTTP utilities for tasker-sequential

---
+++ index.js
@@ -0,0 +1,4 @@
+export { RetryConfig } from './lib/retry-config.js';
+export { fetchWithRetry, createFetchWithRetry } from './lib/fetch-retry.js';
+export { ResponseParseError, parseResponse, parseResponseSafe } from './lib/response-parser.js';
+export { RequestBuilder, buildRequest } from './lib/request-builder.js';
